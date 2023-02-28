667013bb8884a1ab2a086139a8904795a462df24
terlek <terlek@DESKTOP-RL37VNN>
2023-02-28 07:13:53 +0000

    Add basic model, remove weatherForecastControllers

---
+++ Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace CarReviewApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int yearBuilt { get; set; }
+    }
+}
