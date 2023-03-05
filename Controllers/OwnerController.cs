e5e702ef40f6cf51cb2f1a5eef2d5eb61c7aef32
terlek <terlecki1995@gmail.com>
2023-03-05 16:13:04 +0000

    Add owner Controller

---
+++ Controllers/OwnerController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using CarReviewApp.Dto;
+using CarReviewApp.Interfaces;
+using CarReviewApp.Models;
+
+namespace CarReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IMapper _mapper;
+
+        public OwnerController(IOwnerRepository ownerRepository, IMapper mapper)
+        {
+            _ownerRepository = ownerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+        public IActionResult GetOwner()
+        {
+            var owners = _mapper.Map<List<OwnerDto>>(_ownerRepository.GetOwners());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(owners);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOwner(int id)
+        {
+            if (!_ownerRepository.OwnerExists(id))
+            {
+                return NotFound();
+            }
+
+            var owner = _mapper.Map<OwnerDto>(_ownerRepository.GetOwner(id));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(owner);
+        }
+
+        [HttpGet("{id}/car")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCarByOwner(int ownerId)
+        {
+            if(!_ownerRepository.OwnerExists(ownerId))
+            {
+                return NotFound();
+            }
+            
+            var owner = _mapper.Map<List<OwnerDto>>(_ownerRepository.GetCarByOwner(ownerId));
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(owner);
+        }
+
+    }
+}
