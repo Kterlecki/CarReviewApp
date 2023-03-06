ad4df78e84984c1b4dad74b16ae4c4dfad41ecf0
terlek <terlecki1995@gmail.com>
2023-03-06 06:31:38 +0000

    WIP

---
+++ Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarReviewApp.Models;
+
+namespace CarReviewApp.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReview();
+        Review GetReview(int id);
+        ICollection<Review> GetReviewsOfACar(int carId);
+        bool ReviewExists(int id);
+    }
+}
