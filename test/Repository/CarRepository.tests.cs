8156668b2e28c66c581c31dba4eef1b19bc36ab2
Kterlecki <terlecki1995@gmail.com>
2023-03-29 07:15:59 +0100

    add CarRepository Test - CarExists

--- test/Repository/CarRepository.tests.cs
+++ test/Repository/CarRepository.tests.cs
@@ -1,38 +1,60 @@
-// using AutoMapper;
-// using CarReviewApp.Controllers;
-// using CarReviewApp.Data;
-// using CarReviewApp.Dto;
-// using CarReviewApp.Interfaces;
-// using CarReviewApp.Models;
-// using CarReviewApp.Repository;
-// using FakeItEasy;
-// using FluentAssertions;
-// using Microsoft.AspNetCore.Mvc;
-// using Moq;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Xunit;
+using System.Linq;
+using Xunit;
+using FluentAssertions;
+using FakeItEasy;
+using CarReviewApp.Data;
+using CarReviewApp.Repository;
+using Microsoft.EntityFrameworkCore;
+using CarReviewApp.Models;
+using System.Collections.Generic;
 
-// namespace CarReviewApp.tests.Repository;
+public class CarRepositoryTests
+{
+    private readonly DataContext _context;
+    private readonly CarRepository _repository;
 
-// public class CarRepositoryTests
-// {
-//     private readonly Mock<DataContext> _context;
+    public CarRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "test")
+            .Options;
+        _context = new DataContext(options);
+        _repository = new CarRepository(_context);
+    }
+    [Fact]
+    public void CarExists_ShouldReturnTrue_WhenCarExists()
+    {
+        // Arrange
+        var car = new Car{
+            Id = 1,
+            Make = "Audi",
+            Model = "A7",
+            YearBuilt = 2015,
+            Reviews = new List<Review>(),
+            CarOwners = new List<CarOwner>(),
+            CarCategories = new List<CarCategory>()
+        };
+        var carId = 1;
+        _context.Add(car);
+        _context.SaveChanges();
+        // Act
+        var result = _repository.CarExists(carId);
 
-//     public CarRepositoryTests()
-//     {
-//         _context = new Mock<DataContext>();
-//     }
-//     [Fact]
-//     public void CarRepository_CarExists_ReturnsTrue()
-//     {
-//         // Arrange
-//         var id = 1;
-//         var repository = new CarRepository(_context.Object);
-        
-//         // Act
-//         var result = repository.CarExists(id);
-//         // Assert
-//         result.Should().BeTrue();
-//     }
-// }
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void CarExists_ShouldReturnFalse_WhenCarDoesNotExist()
+    {
+        // Arrange
+        int carId = 2;
+
+        // Act
+        var result = _repository.CarExists(carId);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+}
