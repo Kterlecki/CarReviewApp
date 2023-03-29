705b7eabd3e8756c92a3207f0b8cf0cb6c89c291
Kterlecki <terlecki1995@gmail.com>
2023-03-29 08:04:10 +0100

    add CarRepository Test - GetCar

--- test/Repository/CarRepository.tests.cs
+++ test/Repository/CarRepository.tests.cs
@@ -10,33 +10,37 @@ using System.Collections.Generic;
 
 public class CarRepositoryTests
 {
-    private readonly DataContext _context;
     private readonly CarRepository _repository;
 
     public CarRepositoryTests()
     {
-        var options = new DbContextOptionsBuilder<DataContext>()
+        _repository = new CarRepository(GetDbContext());
+    }
+    
+    private static DataContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
             .UseInMemoryDatabase(databaseName: "test")
             .Options;
-        _context = new DataContext(options);
-        _repository = new CarRepository(_context);
-    }
+
+            var databaseContext = new DataContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (!databaseContext.Cars.Any())
+            {
+                    databaseContext.Cars.AddRange(
+                            new Car { Id = 1, Make = "Audi", Model = "A7", YearBuilt = 2015, Reviews = new List<Review>(), CarOwners = new List<CarOwner>(), CarCategories = new List<CarCategory>()},
+                            new Car { Id = 2, Make = "Toyota", Model = "Camry", YearBuilt = 2020 , Reviews = new List<Review>(), CarOwners = new List<CarOwner>(), CarCategories = new List<CarCategory>()},
+                            new Car { Id = 3, Make = "Honda", Model = "Civic", YearBuilt = 2019 , Reviews = new List<Review>(), CarOwners = new List<CarOwner>(), CarCategories = new List<CarCategory>()}
+                        );
+                    databaseContext.SaveChanges();
+            }
+            return databaseContext;
+        }
     [Fact]
     public void CarExists_ShouldReturnTrue_WhenCarExists()
     {
         // Arrange
-        var car = new Car{
-            Id = 1,
-            Make = "Audi",
-            Model = "A7",
-            YearBuilt = 2015,
-            Reviews = new List<Review>(),
-            CarOwners = new List<CarOwner>(),
-            CarCategories = new List<CarCategory>()
-        };
         var carId = 1;
-        _context.Add(car);
-        _context.SaveChanges();
         // Act
         var result = _repository.CarExists(carId);
 
@@ -48,7 +52,7 @@ public class CarRepositoryTests
     public void CarExists_ShouldReturnFalse_WhenCarDoesNotExist()
     {
         // Arrange
-        int carId = 2;
+        int carId = 20;
 
         // Act
         var result = _repository.CarExists(carId);
@@ -56,5 +60,29 @@ public class CarRepositoryTests
         // Assert
         result.Should().BeFalse();
     }
+    [Fact]
+    public void GetCarById_ShouldReturnCar_WhenCarExist()
+    {
+        // Arrange
+        int carId = 1;
 
+        // Act
+        var result = _repository.GetCar(carId);
+
+        // Assert
+        result.Should().BeOfType<Car>();
+        Assert.Matches(result.Make, "Audi");
+    }
+    [Fact]
+    public void GetCarById_ShouldReturnNull_WhenCarDoesntExist()
+    {
+        // Arrange
+        int carId = 10;
+
+        // Act
+        var result = _repository.GetCar(carId);
+
+        // Assert
+        result.Should().BeNull();
+    }
 }
