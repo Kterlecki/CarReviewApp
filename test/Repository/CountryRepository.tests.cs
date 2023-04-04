7061361c5c08c91510445fd089c1061a06230d67
Kterlecki <terlecki1995@gmail.com>
2023-04-04 07:45:44 +0100

    CountryRepositorryTests add In memory Db

--- test/Repository/CountryRepository.tests.cs
+++ test/Repository/CountryRepository.tests.cs
@@ -1,3 +1,50 @@
 
+using System.Linq;
+using Xunit;
+using FluentAssertions;
+using CarReviewApp.Data;
+using CarReviewApp.Repository;
+using Microsoft.EntityFrameworkCore;
+using CarReviewApp.Models;
+using System.Collections.Generic;
+using CarReviewApp.Dto;
 
 namespace CarReviewApp.tests.Repository;
+
+public class CountryRepositoryTests
+{
+    private readonly DataContext _context;
+    private readonly CategoryRepository _repository;
+
+    public CountryRepositoryTests()
+    {
+        _context = GetDbContext();
+        _repository = new CategoryRepository(_context);
+    }
+    private static DataContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "test")
+            .Options;
+
+            var databaseContext = new DataContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (!databaseContext.Countries.Any())
+            {
+                    databaseContext.Countries.AddRange(
+                        new Country { Id = 1, Name = "Country 1",
+                        Owners = new List<Owner>{ new Owner {
+                            Id = 1, Name = "Bob"
+                        }} },
+                        new Country { Id = 2, Name = "Country 2", 
+                        Owners = new List<Owner>() }
+                            );
+                    databaseContext.Owners.Add(
+                        new Owner { Id = 1, Name = "Audi"}
+                    );
+                    databaseContext.SaveChanges();
+            }
+            return databaseContext;
+        }
+    
+}
