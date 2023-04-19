129590cbb28d2309408045dec3d1b73ed9042667
Kterlecki <terlecki1995@gmail.com>
2023-04-19 06:00:23 +0100

    removed commented out code in program.cs

--- client/Program.cs
+++ client/Program.cs
@@ -3,27 +3,11 @@ using CarReviewApp.client.Client;
 using CarReviewApp.client.Service;
 using CarReviewApp.Dto;
 
-// var endPoint = "api/Car";
-// var getCarsEndPoint = "api/Car";
-// var getCarEndPoint = "api/Car/1";
-// var getCarRatingEndPoint = "api/Car/1/rating";
-// var createCarEndPoint = "api/Car?ownerId=1&catId=3";
-// var updateCarEndPoint = "api/Car/3003?catId=3&ownerId=1";
-var deleteCarEndPoint = "api/Car/3003";
-// var carDto = new CarDto{ Make = "Porshce", Model = "911", YearBuilt = 2010 };
-// var carDtoUpdate = new CarDto{ Id = 3003, Make = "Porsche", Model = "Panamera", YearBuilt = 2023 };
-
 var httpClient = new HttpClient();
 var carAppClient = new CarAppClient(httpClient);
 var carService = new CarService(carAppClient);
 carService.ClientHttpHeadersSetUp();
 
-// await carService.GetCars(endPoint);
-// await carService.GetCar(getCarEndPoint);
-// await carService.GetCarRating(getCarRatingEndPoint);
-// await carService.CreateCar(createCarEndPoint, carDto );
-// await carService.UpdateCar(updateCarEndPoint, carDtoUpdate );
-// await carService.DeleteCar( updateCarEndPoint );
 var code = 0;
 
 System.Console.WriteLine("Welcome to the Client App of CarReview");
